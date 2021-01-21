481f15ff8be3074b2d8542ef3795a06f8faf6671
Hector Aristy <h.aristy@gbh.com.do>
2021-01-20 20:23:31 -0400

    feature: Rename user entity to Users.

--- src/framework/entities/User.ts
+++ src/framework/entities/User.ts
@@ -1,7 +1,7 @@
 import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 
-@Entity("users")
-export class User {
+@Entity()
+export class Users {
 
     @PrimaryGeneratedColumn()
     id: number;
