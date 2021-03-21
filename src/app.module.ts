9597bca315a6ebe8f29bc19cee8d64555148efdb
Hector Aristy <h.aristy@gbh.com.do>
2021-03-21 10:44:03 -0400

    Add basic configuration for the typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CompaniesModule } from './companies/companies.module';
+import { CurrenciesModule } from './currencies/currencies.module';
+import { JobsModule } from './jobs/jobs.module';
+import { TagsModule } from './tags/tags.module';
+import { PicturesModule } from './pictures/pictures.module';
+import { LevelsModule } from './levels/levels.module';
+import { JobTypesModule } from './job-types/job-types.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), UsersModule, CompaniesModule, CurrenciesModule, JobsModule, TagsModule, PicturesModule, LevelsModule, JobTypesModule],
   controllers: [AppController],
   providers: [AppService],
 })
