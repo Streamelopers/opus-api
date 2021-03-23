1c2129c75840cdb20d379d04c047205d1637907c
Hector Aristy <h.aristy@gbh.com.do>
2021-03-22 20:20:02 -0400

    remove job-types folder

--- src/job-types/job-types.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JobTypesService } from './job-types.service';
-import { JobTypesController } from './job-types.controller';
-
-@Module({
-  controllers: [JobTypesController],
-  providers: [JobTypesService]
-})
-export class JobTypesModule {}
