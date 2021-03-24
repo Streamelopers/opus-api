cedc28163121a4f168b9ab75a0f9e9005f94937e
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2021-03-24 09:58:40 -0400

    feat: configurated jobTypes module

--- src/jobtypes/jobtypes.module.ts
+++ src/jobtypes/jobtypes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { JobtypesService } from './jobtypes.service';
 import { JobtypesController } from './jobtypes.controller';
+import { Jobtype } from './entities/jobtype.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Jobtype])],
   controllers: [JobtypesController],
   providers: [JobtypesService]
 })
