d327529ad99ca9d2bbc7ec4efa288f740eb7ce22
Enmanuel <enmanuel@torib.io>
2021-03-30 22:29:54 -0400

    Added swagger package and port parameter for db connection

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
+console.log(process.env);
+
 module.exports = {
   type: "postgres",
   host: process.env.POSTGRES_HOST,
-  port: 5432,
+  port: process.env.POSTGRES_PORT,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
