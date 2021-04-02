157bf5e68a0435cad067d503c4e0e756779d80b5
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2021-04-02 11:45:51 -0400

    updated(currencies): updated swagger documentation for currencies controllers

--- src/currencies/dto/create-currency.dto.ts
+++ src/currencies/dto/create-currency.dto.ts
@@ -1,14 +1,21 @@
 import { IsString, IsNotEmpty, Length } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateCurrencyDto {
   @IsString()
   @IsNotEmpty()
   @Length(5, 20)
+  @ApiProperty({
+    minLength: 5,
+    maxLength: 20
+  })
   name: string;
 
   @IsString()
+  @ApiProperty()
   symbol: string;
 
   @IsString()
+  @ApiProperty()
   isoCode: string;
 }
