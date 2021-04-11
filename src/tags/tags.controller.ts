45af4e60719cc3f43e05764c192c2fd2408afa24
Hector Aristy <h.aristy@gbh.com.do>
2021-04-11 14:25:44 -0400

    hotfix: Remove linter warning in the tags context

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -13,8 +13,9 @@ import { TagsService } from "./tags.service";
 import { CreateTagDto } from "./dto/create-tag.dto";
 import { UpdateTagDto } from "./dto/update-tag.dto";
 import { QueryParams } from "../../framework/utils/query";
-import { ApiTags, ApiBody } from "@nestjs/swagger";
+import { ApiTags } from "@nestjs/swagger";
 import { ResponseInterceptor } from "framework/interceptors/response.interceptor";
+import { Tag } from "./entities/tag.entity";
 
 @ApiTags("Tags")
 @Controller("tags")
@@ -23,27 +24,30 @@ export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
   @Post()
-  create(@Body() createTagDto: CreateTagDto) {
+  create(@Body() createTagDto: CreateTagDto): Promise<CreateTagDto> {
     return this.tagsService.create(createTagDto);
   }
 
   @Get()
-  findAll(@Query() params: QueryParams) {
+  findAll(@Query() params: QueryParams): Promise<Tag[]> {
     return this.tagsService.findAll(params);
   }
 
   @Get(":id")
-  findOne(@Param("id") id: string) {
+  findOne(@Param("id") id: string): Promise<Tag> {
     return this.tagsService.findOne(+id);
   }
 
   @Patch(":id")
-  update(@Param("id") id: string, @Body() updateTagDto: UpdateTagDto) {
+  update(
+    @Param("id") id: string,
+    @Body() updateTagDto: UpdateTagDto
+  ): Promise<UpdateTagDto> {
     return this.tagsService.update(+id, updateTagDto);
   }
 
   @Delete(":id")
-  remove(@Param("id") id: string) {
+  remove(@Param("id") id: string): Promise<string> {
     return this.tagsService.remove(+id);
   }
 }
