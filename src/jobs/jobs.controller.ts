b5f3f52752f109a4f414fb9140beb4ecb4513aac
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2021-04-15 21:55:00 -0400

    feat: completed controllers and services

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -7,11 +7,14 @@ import {
   Param,
   Delete,
   UseInterceptors,
+  Query,
 } from "@nestjs/common";
 import { JobsService } from "./jobs.service";
 import { CreateJobDto } from "./dto/create-job.dto";
 import { UpdateJobDto } from "./dto/update-job.dto";
 import { ResponseInterceptor } from "framework/interceptors/response.interceptor";
+import { Job } from "./entities/job.entity";
+import { QueryParams } from "framework/utils/query";
 
 @Controller("jobs")
 @UseInterceptors(ResponseInterceptor)
@@ -24,23 +27,25 @@ export class JobsController {
   }
 
   @Get()
-  findAll() {
-    return this.jobsService.findAll();
+  findAll(@Query() params: QueryParams): Promise<Job[]> {
+    return this.jobsService.findAll(params);
   }
 
   @Get(":id")
-  findOne(@Param("id") id: string) {
+  findOne(@Param("id") id: string): Promise<Job> {
     return this.jobsService.findOne(+id);
   }
 
   @Patch(":id")
-  update(@Param("id") id: string, @Body() updateJobDto: UpdateJobDto) {
-    console.log(updateJobDto);
+  update(
+    @Param("id") id: string,
+    @Body() updateJobDto: UpdateJobDto
+  ): Promise<UpdateJobDto> {
     return this.jobsService.update(+id, updateJobDto);
   }
 
   @Delete(":id")
-  remove(@Param("id") id: string) {
+  remove(@Param("id") id: string): Promise<string> {
     return this.jobsService.remove(+id);
   }
 }
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from "@nestjs/common";
 import { CreateJobDto } from "./dto/create-job.dto";
 import { UpdateJobDto } from "./dto/update-job.dto";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { Like, Repository } from "typeorm";
 import { Job } from "./entities/job.entity";
+import { QueryParams } from "framework/utils/query";
 
 @Injectable()
 export class JobsService {
@@ -18,19 +19,37 @@ export class JobsService {
     return createJobDto;
   }
 
-  findAll() {
-    return this.jobRepository.find();
+  findAll(query: QueryParams): Promise<Job[]> {
+    return this.jobRepository.find({
+      skip: query.page * query.pageSize,
+      take: query.pageSize,
+      where: {
+        isActive: true,
+        title: Like(`%${query.q}%`),
+        description: Like(`%${query.q}%`),
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} job`;
+  findOne(id: number): Promise<Job> {
+    return this.jobRepository.findOne({
+      id,
+      isActive: true,
+    });
   }
 
-  update(id: number, updateJobDto: UpdateJobDto) {
-    return `This action updates a #${id} job`;
+  async update(id: number, updateJobDto: UpdateJobDto): Promise<UpdateJobDto> {
+    await this.jobRepository.update(id, updateJobDto);
+
+    return updateJobDto;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} job`;
+  async remove(id: number): Promise<string> {
+    await this.jobRepository.update(id, {
+      isActive: false,
+      deletedAt: new Date(),
+    });
+
+    return "La publicación fue eliminada de forma correcta";
   }
 }
